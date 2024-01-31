bcb2c2f9bfcbbceb563f38fcc67e12d6f616bf07
mostafa <mostafameerzad@gmail.com>
2024-01-31 20:01:20 +0430

    create nested routes and prevent browser from making requests for the whole page instead handle routing on clientSide

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,19 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./errorPage";
-
+import Contact from "./routes/Contact";
 // todo: create router
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
