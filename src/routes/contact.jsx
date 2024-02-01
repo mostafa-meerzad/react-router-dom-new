ff055451da971fc2a015256815f90d7e3d6813f7
mostafa <mostafameerzad@gmail.com>
2024-02-01 18:59:21 +0430

    put loader function into use and make a working demo

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,14 +1,9 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact} from "../contacts";
+
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const contact = useLoaderData();
 
   return (
     <div id="contact">
@@ -73,4 +68,9 @@ function Favorite({ contact }) {
       </button>
     </Form>
   );
+}
+
+export async function loader({params}){
+  const contact = await getContact(params.contactId)
+  return {contact}
 }
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,18 @@
-import { Outlet, Link } from "react-router-dom";
+import {
+  Outlet,
+  Link,
+  useLoaderData,
+  Form,
+  redirect,
+  NavLink,
+  useNavigation,
+} from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
 
 export default function Root() {
+  const { contacts } = useLoaderData();
+  console.log(contacts);
+  const navigation = useNavigation()
   return (
     <>
       <div id="sidebar">
@@ -17,24 +29,53 @@ export default function Root() {
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isPending, isActive }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                  >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}
+                    {contact.favorite && <span>★</span>}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
-      <div id="detail">
-        <Outlet/>
+      <div id="detail" className={navigation.state === "loading" ? "loading": ""}>
+        <Outlet />
       </div>
     </>
   );
+}
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export async function action() {
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
 }
